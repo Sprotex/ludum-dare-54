b64f722bbf32fb6ee98f08931be984ccd6f04b99
Andy Black <Sprotex@gmail.com>
2023-09-30 17:52:57 +0200

    add bullet particles
    change bullet speed
    add unblocking bullet normal offset
    fix camera rotation limit

--- character/bullet/bulllet_collisions.gd
+++ character/bullet/bulllet_collisions.gd
@@ -1,11 +1,11 @@
 extends Node
 
 @export var character_body: CharacterBody3D
-
+@export var normal_offset_scale = 0.1
 
 func _on_bullet_collision(collision: KinematicCollision3D) -> void:
   var collision_normal = collision.get_normal()
   var travel_direction = collision.get_travel()
   var reflected_direction = travel_direction.bounce(collision_normal)
   character_body.global_transform.basis = Basis.looking_at(reflected_direction)
-
+  character_body.global_transform.origin += collision_normal * normal_offset_scale
--- character/player/camera_rig.gd
+++ character/player/camera_rig.gd
@@ -3,9 +3,9 @@ extends Node3D
 class_name CameraRig
 
 @export var rotation_speed := 1.0
-
+@export var limit_scale = 0.99
 
 func _process(_delta: float) -> void:
   var local_rotation = GameInputs.rotation * rotation_speed * Constants.PIXEL_TO_DEGREE_RATIO
   rotate_x(deg_to_rad(-local_rotation.y))
-  rotation.x = clamp(rotation.x, -TAU * .25, TAU * .25)
+  rotation.x = clamp(rotation.x, -TAU * .25 * limit_scale, TAU * .25 * limit_scale)
