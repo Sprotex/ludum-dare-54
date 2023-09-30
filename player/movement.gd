12991d1ae1d3ad73df68790dd6897a95acddff2b
Andy Black <Sprotex@gmail.com>
2023-09-30 13:04:22 +0200

    fix air movement

--- player/movement.gd
+++ player/movement.gd
@@ -5,7 +5,9 @@ extends Node
 @export var movement_speed := 3.0
 @export var rotation_speed := 1.0
 @export var jump_power := 10.0
-
+@export var friction_multiplier := 0.6
+@export var air_friction_multiplier := 0.9
+@export var air_movement_multiplier := 0.1
 
 func get_gravity_vector() -> Vector3:
   return ProjectSettings.get_setting("physics/3d/default_gravity_vector")
@@ -33,14 +35,23 @@ func calculate_gravity_movement(current_movement: Vector3, _delta: float) -> Vec
 
 func calculate_friction_movement(current_movement: Vector3) -> Vector3:
   current_movement.y += character_body.velocity.y
-  var friction_multiplier = Vector3(0.8, 1.0, 0.8)
-  if character_body.is_on_floor():
-    return current_movement * friction_multiplier
+  var current_friction_multiplier = friction_multiplier
+  if not character_body.is_on_floor():
+    current_friction_multiplier = air_friction_multiplier
+  var movement_multiplier = Vector3(current_friction_multiplier, 1.0, current_friction_multiplier)
+  return current_movement * movement_multiplier
+
+
+func calculate_lateral_movement(current_movement: Vector3) -> Vector3:
+  var current_movement_delta = character_body.global_transform.basis * inputs.movement * movement_speed
+  if not character_body.is_on_floor():
+    current_movement_delta *= air_movement_multiplier
+  current_movement += current_movement_delta
   return current_movement
 
 
 func move(delta: float) -> void:
-  var local_movement = character_body.global_transform.basis * inputs.movement * movement_speed + character_body.velocity
+  var local_movement = calculate_lateral_movement(character_body.velocity)
   local_movement = calculate_jump_movement(local_movement)
   local_movement = calculate_gravity_movement(local_movement, delta)
   local_movement = calculate_friction_movement(local_movement)
