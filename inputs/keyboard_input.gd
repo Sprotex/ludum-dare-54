e6b3acb8a276bfdb53544d1f1f92eb0a5c65524d
Andy Black <Sprotex@gmail.com>
2023-09-30 12:46:02 +0200

    add basic jumping

--- inputs/keyboard_input.gd
+++ inputs/keyboard_input.gd
@@ -4,3 +4,4 @@ extends Node
 func _process(delta: float) -> void:
   GameInputs.movement.x = Input.get_axis("move_left", "move_right")
   GameInputs.movement.z = Input.get_axis("move_forward", "move_backward")
+  GameInputs.movement.y = Input.get_action_strength("jump")
--- player/movement.gd
+++ player/movement.gd
@@ -4,11 +4,46 @@ extends Node
 @export var inputs: CharacterInputs
 @export var movement_speed := 3.0
 @export var rotation_speed := 1.0
+@export var jump_power := 10.0
+
+
+func get_gravity_vector() -> Vector3:
+  return ProjectSettings.get_setting("physics/3d/default_gravity_vector")
+
+
+func get_gravity_magnitude() -> float:
+  return ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func calculate_jump_movement(current_movement: Vector3) -> Vector3:
+  if not character_body.is_on_floor():
+    current_movement.y = 0
+    return current_movement
+  if inputs.movement.y <= 0:
+    current_movement.y = 0
+    return current_movement
+  return current_movement - get_gravity_vector() * jump_power
+
+
+func calculate_gravity_movement(current_movement: Vector3, _delta: float) -> Vector3:
+  if character_body.is_on_floor():
+    return current_movement
+  return current_movement + get_gravity_vector() * get_gravity_magnitude() * _delta
+
+
+func calculate_friction_movement(current_movement: Vector3) -> Vector3:
+  current_movement.y += character_body.velocity.y
+  var friction_multiplier = Vector3(0.8, 1.0, 0.8)
+  if character_body.is_on_floor():
+    return current_movement * friction_multiplier
+  return current_movement
 
 
 func move(delta: float) -> void:
-  var framerate_independent_movement = inputs.movement * movement_speed 
-  var local_movement = character_body.global_transform.basis * framerate_independent_movement
+  var local_movement = character_body.global_transform.basis * inputs.movement * movement_speed + character_body.velocity
+  local_movement = calculate_jump_movement(local_movement)
+  local_movement = calculate_gravity_movement(local_movement, delta)
+  local_movement = calculate_friction_movement(local_movement)
   character_body.velocity = local_movement
   character_body.move_and_slide()
 
