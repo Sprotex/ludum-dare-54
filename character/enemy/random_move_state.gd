616a1e5bbfaf8c373c724199ea9ea898ad40361e
Andy Black <Sprotex@gmail.com>
2023-10-01 11:50:02 +0200

    fix turn in random move state

--- character/enemy/random_move_state.gd
+++ character/enemy/random_move_state.gd
@@ -2,13 +2,13 @@ extends AIState
 
 @export var character_movement: CharacterMovement
 @export var inputs: CharacterInputs
+@export var max_walk_distance = 10.0
 
-var max_walk_distance = 10.0
 @onready var random = RandomNumberGenerator.new()
 
 
 func _handle_ai_state_enabled() -> void:
-  var random_rotation = random.randf_range(-TAU, TAU)
+  var random_rotation = random.randf_range(-TAU * .5, TAU * .5)
   var random_distance = random.randf_range(0.0, max_walk_distance)
   turn(inputs, character_movement, random_rotation)
   await on_turn_completed
