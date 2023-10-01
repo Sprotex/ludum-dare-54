c92c2147931ee565d3e5976b452f0424cfd6c1e5
Andy Black <Sprotex@gmail.com>
2023-10-01 12:21:26 +0200

    fix reloading scene with dynamic objects

--- autoload/message_bus/message_bus.gd
+++ autoload/message_bus/message_bus.gd
@@ -1,10 +1,13 @@
 extends Node
 
 signal on_player_died
+signal on_player_health_changed(health)
 signal on_enemy_died
 signal on_scene_reloaded
 signal on_score_changed(score)
 signal on_player_shot(reload_time)
 
+
+signal on_object_created(object)
 signal on_player_object_requested
 signal on_player_object_broadcast(player)
--- weapon/weapon.gd
+++ weapon/weapon.gd
@@ -13,7 +13,7 @@ var is_reloading := false
 
 func shoot() -> void:
   var bullet_instance = bullet.instantiate()
-  get_tree().get_root().add_child(bullet_instance)
+  MessageBus.on_object_created.emit(bullet_instance)
   bullet_instance.global_position = firepoint.global_position
   bullet_instance.global_rotation = firepoint_direction.global_rotation
   if character_body.type == Enums.CharacterType.PLAYER:
