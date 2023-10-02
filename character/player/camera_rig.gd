fd35722064d2a4e16d4a7a61b2b298a8ecab56bd
Andy Black <Sprotex@gmail.com>
2023-10-02 20:12:38 +0200

    add camera swapping,fix camera raycast into player

--- character/player/camera_rig.gd
+++ character/player/camera_rig.gd
@@ -7,13 +7,22 @@ class_name CameraRig
 @export var raycast: RayCast3D
 @export var camera: Camera3D
 @export var inputs: PlayerInputs
+@export var character_body: Character
 
 @onready var camera_starting_point = camera.transform.origin
+@onready var camera_side = 1
+
+
+func _ready() -> void:
+  raycast.add_exception(character_body)
 
 
 func _set_camera_position() -> void:
   if inputs.camera_swap:
-    transform.origin.x = -transform.origin.x
+    camera_side *= -1
+    var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+    var target_position = Vector3(camera_side, position.y, position.z)
+    tween.tween_property(self, "position", target_position, 0.5)
   if raycast.is_colliding():
     camera.global_transform.origin = raycast.get_collision_point()
   else:
