8c83a69bb0980686cf7d02ca992d2066007ae1c8
Andy Black <Sprotex@gmail.com>
2023-10-02 18:49:25 +0200

    fix laser sight and bullet spawning

--- character/player/copy_x_rotation.gd
+++ character/player/copy_x_rotation.gd
@@ -2,5 +2,6 @@ extends Node3D
 
 @export var rotation_source: Node3D
 
-func _process(_delta):
-  global_rotation.x = rotation_source.global_rotation.x
+
+func _process(_delta) -> void:
+  (func(): global_rotation.x = rotation_source.global_rotation.x).call_deferred()
